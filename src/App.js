580e76bd9deb18d2549a363af9cb1472b8631606
cmdrGuyson <gayangakuruppu@gmail.com>
2020-11-11 02:32:52 +0530

    [Implemented] Login UI

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import login from "./pages/login/login";
 import { Fragment } from "react";
 
 function App() {
-	return (
-		<Fragment>
-			<Router>
-				<Switch>
-					<Route exact path="/" component={home} />
-					<Route exact path="/login" component={login} />
-				</Switch>
-			</Router>
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={home} />
+          <Route exact path="/login" component={login} />
+        </Switch>
+      </Router>
+    </Fragment>
+  );
 }
 
 export default App;
