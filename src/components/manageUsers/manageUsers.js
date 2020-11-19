26ac072ea76bca025a2f77d782153b051d487e14
Nethmi Rodrigo <neth23rodrigo@gmail.com>
2020-11-19 22:27:45 +0530

    Search function

--- src/components/manageUsers/manageUsers.js
+++ src/components/manageUsers/manageUsers.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import {
-  InputGroup,
-  Card,
-  Button,
-  Col,
-  Row,
-  FormControl,
-  CardColumns,
+	InputGroup,
+	Card,
+	Button,
+	Col,
+	Row,
+	FormControl,
+	CardColumns,
 } from "react-bootstrap";
 import PropTypes from "prop-types";
 
@@ -19,86 +19,101 @@ import { connect } from "react-redux";
 import { getAllUsers } from "../../redux/actions/dataActions";
 
 function ManageUsers(props) {
-  const [_users, setUsers] = useState([]);
+	const [_users, setUsers] = useState([]);
 
-  const {
-    data: { users, loading },
-  } = props;
+	const {
+		data: { users, loading },
+	} = props;
 
-  useEffect(() => {
-    props.getAllUsers();
-  }, []);
+	useEffect(() => {
+		props.getAllUsers();
+	}, []);
 
-  useEffect(() => {
-    users && setUsers(users);
-  }, [users]);
+	useEffect(() => {
+		users && setUsers(users);
+		console.log(users);
+	}, [users]);
 
-  let usersMarkup = !loading ? (
-    _users.map((user) => <UserCard key={user._id} user={user} />)
-  ) : (
-    <p>Loading...</p>
-  );
+	let usersMarkup = !loading ? (
+		_users.map((user) => <UserCard key={user._id} user={user} />)
+	) : (
+		<p>Loading...</p>
+	);
 
-  return (
-    <div>
-      <Card className="search-box-users">
-        <Card.Body>
-          <Card.Title>Search Users</Card.Title>
-          <Row>
-            <Col xs={5}>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text>
-                    <i className="fas fa-search"></i>
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  placeholder="Search by name"
-                  aria-label="Search by name"
-                  aria-describedby="basic-addon2"
-                />
-              </InputGroup>
-            </Col>
-            <Col xs={7}>
-              <Button
-                className="search-user-button"
-                variant="outline-primary"
-                active
-              >
-                All Users
-              </Button>{" "}
-              <Button
-                className="search-user-button"
-                variant="outline-secondary"
-              >
-                Non-verified
-              </Button>{" "}
-              <Button className="search-user-button" variant="outline-success">
-                Premium
-              </Button>{" "}
-              <Button className="search-user-button" variant="outline-danger">
-                Blacklisted
-              </Button>{" "}
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
+	const search = (input) => {
+		let usersCopy = users.map((user) => user);
+		const lowercasedInput = input.toLowerCase();
+		const searchKeys = ["email", "firstName", "lastName"];
+		const result = usersCopy.filter((item) => {
+			return Object.keys(item).some((key) => {
+				if (searchKeys.includes(key)) {
+					return item[key].toLowerCase().includes(lowercasedInput);
+				}
+			});
+		});
+		setUsers(result);
+	};
 
-      <CardColumns>{usersMarkup}</CardColumns>
-    </div>
-  );
+	return (
+		<div>
+			<Card className="search-box-users">
+				<Card.Body>
+					<Card.Title>Search Users</Card.Title>
+					<Row>
+						<Col xs={5}>
+							<InputGroup>
+								<InputGroup.Prepend>
+									<InputGroup.Text>
+										<i className="fas fa-search"></i>
+									</InputGroup.Text>
+								</InputGroup.Prepend>
+								<FormControl
+									placeholder="Search by name"
+									aria-label="Search by name"
+									aria-describedby="basic-addon2"
+									onChange={(e) => search(e.target.value)}
+								/>
+							</InputGroup>
+						</Col>
+						<Col xs={7}>
+							<Button
+								className="search-user-button"
+								variant="outline-primary"
+								active
+							>
+								All Users
+							</Button>{" "}
+							<Button
+								className="search-user-button"
+								variant="outline-secondary"
+							>
+								Non-verified
+							</Button>{" "}
+							<Button className="search-user-button" variant="outline-success">
+								Premium
+							</Button>{" "}
+							<Button className="search-user-button" variant="outline-danger">
+								Blacklisted
+							</Button>{" "}
+						</Col>
+					</Row>
+				</Card.Body>
+			</Card>
+			<CardColumns>{usersMarkup}</CardColumns>
+		</div>
+	);
 }
 
 ManageUsers.propTypes = {
-  getAllUsers: PropTypes.func.isRequired,
+	getAllUsers: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  data: state.data,
+	data: state.data,
 });
 
 const mapActionsToProps = {
-  getAllUsers,
+	getAllUsers,
 };
 
 export default connect(mapStateToProps, mapActionsToProps)(ManageUsers);
