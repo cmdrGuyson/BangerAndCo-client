bc7cd44ac89ea94cf5c527302df4e05f6660bcbb
Nethmi Rodrigo <neth23rodrigo@gmail.com>
2020-12-02 15:24:19 +0530

    Nothing really

--- src/components/manageVehicles/manageVehicles.js
+++ src/components/manageVehicles/manageVehicles.js
@@ -1,64 +1,105 @@
 import React, { useState, useEffect } from "react";
 import {
-  InputGroup,
-  Card,
-  Button,
-  Col,
-  Row,
-  FormControl,
-  CardColumns,
-  Alert,
+	InputGroup,
+	Card,
+	Button,
+	Col,
+	Row,
+	FormControl,
+	CardColumns,
+	Alert,
+	Dropdown,
 } from "react-bootstrap";
 import PropTypes from "prop-types";
 
 export default function manageVehicles() {
-  return (
-    <div>
-      <Card className="search-box-users">
-        <Card.Body>
-          <Card.Title>Search Vehicles</Card.Title>
-          <Row>
-            <Col xs={5}>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text>
-                    <i className="fas fa-search"></i>
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  placeholder="Search by name"
-                  aria-label="Search by name"
-                  aria-describedby="basic-addon2"
-                />
-              </InputGroup>
-            </Col>
-            <Col xs={7}>
-              <Button className="search-user-button" variant="outline-primary">
-                All Users
-              </Button>{" "}
-              <Button
-                className="search-user-button"
-                variant="outline-secondary"
-              >
-                Non-verified
-              </Button>{" "}
-              <Button className="search-user-button" variant="outline-success">
-                Premium
-              </Button>{" "}
-              <Button className="search-user-button" variant="outline-danger">
-                Blacklisted
-              </Button>{" "}
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
-      {/* {_users.length > 0 ? (
+	return (
+		<div>
+			<Card className="search-box-users">
+				<Card.Body>
+					<Card.Title>Search Vehicles</Card.Title>
+					<Row>
+						<Col xs={5}>
+							<InputGroup>
+								<InputGroup.Prepend>
+									<InputGroup.Text>
+										<i className="fas fa-search"></i>
+									</InputGroup.Text>
+								</InputGroup.Prepend>
+								<FormControl
+									placeholder="Search by name"
+									aria-label="Search by name"
+									aria-describedby="basic-addon2"
+								/>
+							</InputGroup>
+						</Col>
+						<Col xs={7}>
+							<Row>
+								<Col>
+									<Dropdown>
+										<Dropdown.Toggle
+											variant="outline-secondary"
+											id="dropdown-basic"
+										>
+											Transmission
+										</Dropdown.Toggle>
+										<Dropdown.Menu>
+											<Dropdown.Item>Auto</Dropdown.Item>
+											<Dropdown.Item>Manual</Dropdown.Item>
+										</Dropdown.Menu>
+									</Dropdown>
+								</Col>
+								<Col>
+									<Dropdown>
+										<Dropdown.Toggle
+											variant="outline-secondary"
+											id="dropdown-basic"
+										>
+											Fuel
+										</Dropdown.Toggle>
+										<Dropdown.Menu>
+											<Dropdown.Item>Diesel</Dropdown.Item>
+											<Dropdown.Item>Petrol</Dropdown.Item>
+										</Dropdown.Menu>
+									</Dropdown>
+								</Col>
+								<Col>
+									<Dropdown>
+										<Dropdown.Toggle
+											variant="outline-secondary"
+											id="dropdown-basic"
+										>
+											Vehicle Type
+										</Dropdown.Toggle>
+										<Dropdown.Menu>
+											<Dropdown.Item>Diesel</Dropdown.Item>
+											<Dropdown.Item>Petrol</Dropdown.Item>
+										</Dropdown.Menu>
+									</Dropdown>
+								</Col>
+								<Col>
+									<Dropdown>
+										<Dropdown.Toggle variant="danger" id="dropdown-basic">
+											Availability
+										</Dropdown.Toggle>
+										<Dropdown.Menu>
+											<Dropdown.Item>Available</Dropdown.Item>
+											<Dropdown.Item>Unavailable</Dropdown.Item>
+										</Dropdown.Menu>
+									</Dropdown>
+								</Col>
+							</Row>
+						</Col>
+					</Row>
+				</Card.Body>
+			</Card>
+			{/* {_users.length > 0 ? (
         <CardColumns>{usersMarkup}</CardColumns>
       ) : loading ? (
         <p>Loading...</p>
       ) : (
         <Alert variant="warning">No users found!</Alert>
       )} */}
-    </div>
-  );
+		</div>
+	);
 }
--- src/components/manageVehicles/viewVehicle.js
+++ src/components/manageVehicles/viewVehicle.js
@@ -1,51 +1,55 @@
 import React from "react";
 import {
-  Card,
-  Row,
-  Col,
-  Badge,
-  ListGroup,
-  Button,
-  ButtonGroup,
+	Card,
+	Row,
+	Col,
+	Badge,
+	ListGroup,
+	Button,
+	ButtonGroup,
 } from "react-bootstrap";
+import "./viewVehicle.scss";
 
 export default function viewVehicle() {
-  return (
-    <Card className="view-user-card">
-      <Card.Body>
-        <Card.Img
-          variant="top"
-          src="http://localhost:5000/vehicles/default_car.jpg"
-        />
+	return (
+		<Card className="view-user-card">
+			<Card.Body>
+				<div
+					className="vehicle-image"
+					style={{
+						backgroundImage:
+							"src(http://localhost:5000/vehicles/default_car.jpg)",
+					}}
+				></div>
 
-        <hr />
+				<hr />
 
-        <ListGroup>
-          <ListGroup.Item variant="light">
-            <Badge variant="secondary">NIC number</Badge>
-            <span> Name</span>
-          </ListGroup.Item>
-          <ListGroup.Item variant="light">
-            <Badge variant="secondary">Driver's license number</Badge>
-            <span> Blame</span>
-          </ListGroup.Item>
-          <ListGroup.Item variant="light">
-            <Badge variant="secondary">Contact number</Badge>
-            <span> Flame</span>
-          </ListGroup.Item>
-          <ListGroup.Item variant="light">
-            <Badge variant="secondary">Date of birth</Badge>
-            <span> Game</span>
-          </ListGroup.Item>
-        </ListGroup>
-      </Card.Body>
+				<ListGroup>
+					<ListGroup.Item variant="light">
+						<Badge variant="secondary">Chassis number</Badge>
+						<span> Name</span>
+					</ListGroup.Item>
+					<ListGroup.Item variant="light">
+						<Badge variant="secondary">License number</Badge>
+						<span> Blame</span>
+					</ListGroup.Item>
+					<ListGroup.Item variant="light">
+						<Badge variant="secondary">Mileage</Badge>
+						<span> Flame</span>
+					</ListGroup.Item>
+					<ListGroup.Item variant="light">
+						<Badge variant="secondary">Registration number</Badge>
+						<span> Game</span>
+					</ListGroup.Item>
+				</ListGroup>
+			</Card.Body>
 
-      <Card.Footer>
-        {" "}
-        <small className="text-muted">
-          {/* {`Registered on ${dayjs(user.createdAt).toString()}`} */}
-        </small>
-      </Card.Footer>
-    </Card>
-  );
+			<Card.Footer>
+				{" "}
+				<small className="text-muted">
+					{/* {`Registered on ${dayjs(user.createdAt).toString()}`} */}
+				</small>
+			</Card.Footer>
+		</Card>
+	);
 }
